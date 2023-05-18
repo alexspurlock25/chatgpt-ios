35091b1b3575633a0959bed5f5346c17a97e453a
Alexander Spurlock <alexanderspurlock@gmail.com>
2023-05-18 06:54:57 -0400

    Initial Commit

---
+++ ChatGPT/ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTApp.swift
+//  ChatGPT
+//
+//  Created by Alexander Spurlock on 5/18/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
